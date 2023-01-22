3455557263fc4b5e720b5a30c29c9ef38a18e315
Yasir Qureshi <yasirqureshi@Yasirs-MacBook-Air.local>
2023-01-22 11:53:50 -0500

    added error logging feature

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from flask import Flask
 from config import Config
 from .auth.routes import auth
@@ -7,6 +8,8 @@ from .models import db, login
 from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_mail import Mail, Message
+from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 
@@ -24,13 +27,27 @@ login.login_view = 'auth.login'
 login.login_message = 'Please log in to see this page.'
 login.login_message_category = 'danger'
 
-app.config['SECRET_KEY'] = 'top-secret!'
-app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'apikey'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.environ.get('MAIL_PORT')
+app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS')
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
+app.config['ADMINS'] = os.environ.get('ADMINS')
 mail = Mail(app)
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/beatbox.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Beatbox startup')
 
 from . import routes
