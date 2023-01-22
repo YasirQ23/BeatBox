8ebbb784a76f0819c1b9cfb7f55a5028c4a60177
Yasir Qureshi <yasirqureshi@Yasirs-MacBook-Air.local>
2023-01-22 10:59:24 -0500

    error css/error email ft added

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from config import Config
 from .auth.routes import auth
@@ -5,6 +6,7 @@ from .auth.routes import auth
 from .models import db, login
 from flask_migrate import Migrate
 from flask_moment import Moment
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
@@ -21,5 +23,14 @@ login.init_app(app)
 login.login_view = 'auth.login'
 login.login_message = 'Please log in to see this page.'
 login.login_message_category = 'danger'
+
+app.config['SECRET_KEY'] = 'top-secret!'
+app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'apikey'
+app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
+mail = Mail(app)
 
 from . import routes
